9306601ef29cc011344fa7da55176bed196074c8
Luke Thompson <luke@yorkbay.net.nz>
2021-08-23 20:29:34 +1200

    initial commit

---
+++ media.js
@@ -0,0 +1,44 @@
+/*
+Basic layout:
+
+ * Get list of media file names
+ * Name should include date
+ * Order by date descending
+ * Make template for each media file type
+ * Add to body in order
+*/
+
+let listicle = document.getElementById('listOfAllLists');
+
+async function get_media_names() {
+    let result = [];
+    let names = [];
+
+    await fetch("media/list.txt")
+        .then(response => response.text())
+        .then(data => {
+            console.log(data.split(","));
+            names = data.split(",");
+        });
+
+    names.forEach(item => {
+        let test_image = '<img src="media/'+ item +'" class="media">';
+        result.push(test_image);
+    });
+
+    console.log(result);
+    return result;
+}
+
+get_media_names().then(result => {
+    let insert = "";
+
+    // more complicated handling goes here
+    result.forEach(img => {
+        insert = insert.concat(img);
+    })
+
+    console.log(insert);
+    
+    listicle.innerHTML = insert;
+});
